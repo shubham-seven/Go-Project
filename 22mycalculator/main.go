26ffb43ba061cdd2d54d09ce15ecc72f1e840369
“shubham10021992” <shubhamrolly@gmail.com>
2023-03-14 12:32:17 +0530

    made changes using scanf

--- 22mycalculator/main.go
+++ 22mycalculator/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	//"strings"
 	// "math"
-	"bufio"
+	//"bufio"
 	"os"
-	"strconv"
+	//"strconv"
 )
 
 func main() {
 	fmt.Println("Welcome to my calculator")
-	
-	reader1 := bufio.NewReader(os.Stdin)
+	var float1,float2 float64
+	fmt.Println("Enter first number:")
+
+	_,err1:=fmt.Scanf("%f",&float1)
+    CheckErrorNil(err1)
+	fmt.Println("Enter Second number:")
+
+	_,err2:=fmt.Scanf("%f",&float2)
+	CheckErrorNil(err2)
+
+	/*reader1 := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter first number:")
 	val1,_:=reader1.ReadString('\n')
 	float1,err:=strconv.ParseFloat(strings.TrimSpace(val1),64)
@@ -24,14 +33,15 @@ func main() {
 	fmt.Println("Enter Second number:")
 	val2,_:=reader2.ReadString('\n')
     float2,err:=strconv.ParseFloat(strings.TrimSpace(val2),64)
-    CheckErrorNil(err)
+    CheckErrorNil(err)*/
 
 	Sum := float1 + float2
-	fmt.Printf("Here is the Sum of two numbers: %f", Sum)
+	fmt.Printf("Here is the Sum of two numbers: %f\n", Sum)
 
 }
 func CheckErrorNil(err error) {
 	if err != nil {
-		panic("Invalid Syntax")
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
