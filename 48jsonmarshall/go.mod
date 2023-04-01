32542d7ef7a6ccb1ea4dae8ee0d2f1dc2a42f8a1
shubham10021992 <shubhamrolly@gmail.com>
2023-04-01 13:55:19 +0530

    marshalling

---
+++ 48jsonmarshall/go.mod
@@ -0,0 +1,3 @@
+module jsonmarshal
+
+go 1.20
--- 48jsonmarshall/main.go
+++ 48jsonmarshall/main.go
@@ -1,10 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	//"os"
+	//"strconv"
+)
 
-type person struct{
-value int
+type ColourGroup struct {
+	ID    int
+	Name  string
+	Color string
 }
-func main(){
-	
+
+func main() {
+/*
+	group1 := ColourGroup{
+		ID:    1,
+		Name:  "shubham",
+		Color: "Red",
+	}
+	group2 := ColourGroup{
+		ID:    2,
+		Name:  "sumit",
+		Color: "black",
+	}
+	p, _ := strconv.ParseFloat("123.9079087", 64)
+	fmt.Println(p)
+	fmt.Println(group1)
+	fmt.Println(group2)
+
+	x,err := json.Marshal(group1)
+	if err!=nil{
+		fmt.Println("error:",err)
+	}
+	os.Stdout.Write(x)
+	*/
+
+//Unmarshall json 
+var jsonblob = []byte(`[
+{"ID":    1,"Name":  "shubham","Color": "Red"},
+{"ID":    2,"Name":  "sumit","Color": "black"}
+]`)
+var cgr []ColourGroup
+err1:=json.Unmarshal(jsonblob,&cgr)
+if err1!=nil{
+	fmt.Println(err1)
+}
+fmt.Printf("%+v",cgr)
 }
